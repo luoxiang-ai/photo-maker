9872f95642cd7cd81df0ee2f22d3719424281700
luoxiang <luoxiang2b@163.com>
2024-01-24 00:38:04 +0800

    固定header头部

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,9 @@
         <el-header style="padding: 0;">
             <app-header />
         </el-header>
-
+        
         <!-- 中间 -->
-        <el-main style="padding: 0;min-height: 1000px;">
+        <el-main style="padding: 0;min-height: 1000px;margin-top: 60px;">
             <slot />
         </el-main>
 
@@ -15,6 +15,7 @@
             <app-footer />
         </el-footer>
     </el-container>
+    
 
     <el-backtop :right="100" :bottom="100" />
 </template>
@@ -24,6 +25,16 @@
 </script>
 
 <style scoped>
+.el-header {
+  position: fixed;
+  width: 100%;
+  z-index: 1000;
+}
+/* .el-main,
+.el-footer {
+  padding-top: 100px;
+  z-index: 999;
+} */
 /* >>>.el-main{
     padding-left: 0px;
     padding-right: 0px;
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,8 @@
     <section class="relative py-10 overflow-hidden bg-black sm:py-16 lg:py-24 xl:py-32">
         <div class="absolute inset-0">
             <!-- <el-image class="object-cover w-full h-full md:object-left md:scale-150 md:origin-top-left" src="/1.jpg" lazy alt="Style photo" /> -->
-            <img class="object-cover w-full h-full md:object-left md:scale-150 md:origin-top-left" src="/1.jpg" alt="Style photo">
+            <img class="object-cover w-full h-full md:object-left md:scale-150 md:origin-top-left" src="/1.jpg"
+                alt="Style photo">
         </div>
 
         <div class="absolute inset-0 hidden bg-gradient-to-r md:block from-black to-transparent"></div>
@@ -11,7 +12,8 @@
 
         <div class="relative px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
             <div class="text-center md:w-2/3 lg:w-1/2 xl:w-1/3 md:text-left">
-                <h1 class="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl">Style photo - Quickly generate styled photos</h1>
+                <h1 class="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl">Style photo - Quickly
+                    generate styled photos</h1>
                 <p class="mt-4 text-base text-gray-200">Upload a photo to customize a lifelike human image.</p>
 
                 <div class="mt-8 lg:mt-12">
@@ -23,7 +25,8 @@
         </div>
     </section>
 
-    <iframe src="https://tencentarc-photomaker.hf.space" scrolling="no" ref="iframe" class="iframe" id="iframe"></iframe>
+    <iframe src="https://tencentarc-photomaker.hf.space" scrolling="no" ref="iframe" class="h-[2400px] w-full"
+        id="iframe"></iframe>
 
     <el-row>
         <el-col class="mt-8" :offset="3" :span="18">
@@ -31,7 +34,8 @@
                 <template #header>
                     <div>
                         <h2 class="text-4xl pb-5 font-bold">Style photo - Transform your photos with unique styles.</h2>
-                        <p class="text-gray-500">We demonstrated the photo creation features under basic guidance for PhotoMaker. Adjustment prompts are displayed beneath each image.</p>
+                        <p class="text-gray-500">We demonstrated the photo creation features under basic guidance for
+                            PhotoMaker. Adjustment prompts are displayed beneath each image.</p>
                     </div>
                 </template>
 
@@ -42,8 +46,12 @@
             <el-card :body-style="{ padding: '6px' }">
                 <template #header>
                     <div>
-                        <h2 class="text-4xl pb-5 font-bold">Style photo - Bring characters from artworks or old photos into reality.</h2>
-                        <p class="text-gray-500">By using artistic paintings, sculptures, or old photos of individuals as input, our PhotoMaker can transport a person from the last century or even ancient times into the present century, creating a 'photograph' for them. Adjustment prompts are displayed beneath each image.</p>
+                        <h2 class="text-4xl pb-5 font-bold">Style photo - Bring characters from artworks or old photos into
+                            reality.</h2>
+                        <p class="text-gray-500">By using artistic paintings, sculptures, or old photos of individuals as
+                            input, our PhotoMaker can transport a person from the last century or even ancient times into
+                            the present century, creating a 'photograph' for them. Adjustment prompts are displayed beneath
+                            each image.</p>
                     </div>
                 </template>
 
@@ -55,7 +63,8 @@
                 <template #header>
                     <div>
                         <h2 class="text-4xl pb-5 font-bold">Style photo - Stylization.</h2>
-                        <p class="text-gray-500">Our PhotoMaker not only generates realistic human photos but also stylizes them while preserving ID attributes. Adjustment prompts are displayed in the first row.</p>
+                        <p class="text-gray-500">Our PhotoMaker not only generates realistic human photos but also stylizes
+                            them while preserving ID attributes. Adjustment prompts are displayed in the first row.</p>
                     </div>
                 </template>
 
@@ -67,7 +76,8 @@
                 <template #header>
                     <div>
                         <h2 class="text-4xl pb-5 font-bold">Style photo - Alter age or gender.</h2>
-                        <p class="text-gray-500">By simply replacing category terms, such as men and women, our method achieves changes in gender and age while preserving the original identity.</p>
+                        <p class="text-gray-500">By simply replacing category terms, such as men and women, our method
+                            achieves changes in gender and age while preserving the original identity.</p>
                     </div>
                 </template>
 
@@ -79,16 +89,20 @@
                 <template #header>
                     <div>
                         <h2 class="text-4xl pb-5 font-bold">Style photo - Identity blending.</h2>
-                        <p class="text-gray-500">If users provide images with different IDs as input, our PhotoMaker adeptly integrates features from various IDs to form a new, blended identity.</p>
+                        <p class="text-gray-500">If users provide images with different IDs as input, our PhotoMaker adeptly
+                            integrates features from various IDs to form a new, blended identity.</p>
                     </div>
                 </template>
 
                 <el-image src="/identity_mixing.jpg" lazy alt="Style photo" />
 
-                <p class="mt-4 mb-4 text-gray-500">For identity blending, our method can adjust the merging ratio by controlling the percentage of identity images in the input image pool or through weighted prompts.</p>
-                <p class="mt-4 mb-4 text-gray-500">We first demonstrate how our method customizes a new identity by controlling the proportions of different IDs in the input image pool.</p>
+                <p class="mt-4 mb-4 text-gray-500">For identity blending, our method can adjust the merging ratio by
+                    controlling the percentage of identity images in the input image pool or through weighted prompts.</p>
+                <p class="mt-4 mb-4 text-gray-500">We first demonstrate how our method customizes a new identity by
+                    controlling the proportions of different IDs in the input image pool.</p>
                 <el-image lazy src="/change_mix_ratio_image.jpg" alt="Style photo" />
-                <p class="mt-4 mb-4 text-gray-500">Then, we multiply the embeddings corresponding to images associated with specific IDs by a coefficient to control their influence in blending into the new identity.</p>
+                <p class="mt-4 mb-4 text-gray-500">Then, we multiply the embeddings corresponding to images associated with
+                    specific IDs by a coefficient to control their influence in blending into the new identity.</p>
                 <el-image lazy src="/change_mix_ratio_embed.jpg" alt="Style photo" />
 
             </el-card>
@@ -98,7 +112,10 @@
                 <template #header>
                     <div>
                         <h2 class="text-4xl pb-5 font-bold">Style photo - Comparison.</h2>
-                        <p class="text-gray-500">Compared to other methods, our PhotoMaker excels in delivering high-quality and diverse generation capabilities, editability, efficient inference, and strong ID fidelity. More comparative results can be found in our research paper. We display conditioning prompts in the second column.</p>
+                        <p class="text-gray-500">Compared to other methods, our PhotoMaker excels in delivering high-quality
+                            and diverse generation capabilities, editability, efficient inference, and strong ID fidelity.
+                            More comparative results can be found in our research paper. We display conditioning prompts in
+                            the second column.</p>
                     </div>
                 </template>
 
@@ -120,17 +137,11 @@ useSeoMeta({
 });
 
 const scrollTo = () => {
-      const element = document.getElementById("iframe");
-      if (element) {
+    const element = document.getElementById("iframe");
+    if (element) {
         element.scrollIntoView({ behavior: 'smooth' });
-      }
     }
+}
 </script>
 
-<style>
-.iframe {
-    width: 100%;
-    height: 2400px;
-    border: none;
-}
-</style>
+<style></style>
